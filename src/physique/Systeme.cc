f830eb77953dc4f9d99d318caa9ff12f58e00316
AlbinSmiley <teleny@duck.com>
2026-03-28 16:21:13 +0100

    Restructuration du projet

---
+++ src/physique/Systeme.cc
@@ -0,0 +1,51 @@
+#include "Systeme.h"
+#include "TextViewer.h"
+
+using namespace std;
+
+// accesseurs
+vector<Particule> const &Systeme::getParticules() const { return particules; }
+
+vector<Obstacle *> const &Systeme::getObstacles() const { return obstacles; }
+
+vector<Source *> const &Systeme::getSources() const { return sources; }
+
+// ajouts
+void Systeme::ajouteParticule(Particule const &p) { particules.push_back(p); }
+
+void Systeme::ajouteObstacle(Obstacle *o) { obstacles.push_back(o); }
+
+void Systeme::ajouteSource(Source *s) { sources.push_back(s); }
+
+// affichage
+// c'est l'opérateur d'affichage
+ostream &operator<<(ostream &out, Systeme const &s) {
+
+  TextViewer vue(out); // outil d'afficahge
+  out << "Le systeme est constitue des " << s.getParticules().size()
+      << " particules suivantes :" << endl;
+
+  for (auto const &p : s.getParticules()) {
+    out << p << endl;
+  }
+
+  out << "et de(s) " << s.getObstacles().size()
+      << " obstacles suivants :" << endl;
+
+  for (auto const &o : s.getObstacles()) {
+    (*o).dessine_sur(vue); // c'est du polymorphsime
+    out << endl;
+  }
+
+  if (!s.getSources().empty()) {
+    out << "et de(s) " << s.getSources().size()
+        << " sources suivantes :" << endl;
+
+    for (auto const &source : s.getSources()) {
+      (*source).dessine_sur(vue);
+      out << *source << endl;
+    }
+  }
+
+  return out;
+}
